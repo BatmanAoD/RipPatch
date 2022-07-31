82d02dd7ebdc8a6319d01e72265da3d0b57cf621
Kyle Strand <kyle.strand@pieinsurance.com>
2022-07-31 17:21:09 -0600

    bring over patch printer from ripgrep branch

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::process;
+
+mod patch;
+mod patcht;
+
+fn main() {
+    if let Err(err) = Args::parse().and_then(search_parallel) {
+        eprintln!("{}", err);
+        process::exit(2);
+    }
+} 
+
+// XXX copied from rg
+fn search_parallel(args: &Args) -> Result<bool> {
+    use std::sync::atomic::AtomicBool;
+    use std::sync::atomic::Ordering::SeqCst;
+
+    let quit_after_match = args.quit_after_match()?;
+    let started_at = Instant::now();
+    let subject_builder = args.subject_builder();
+    let bufwtr = args.buffer_writer()?;
+    let stats = args.stats()?.map(Mutex::new);
+    let matched = AtomicBool::new(false);
+    let searched = AtomicBool::new(false);
+    let mut searcher_err = None;
+    args.walker_parallel()?.run(|| {
+        let bufwtr = &bufwtr;
+        let stats = &stats;
+        let matched = &matched;
+        let searched = &searched;
+        let subject_builder = &subject_builder;
+        let mut searcher = match args.search_worker(bufwtr.buffer()) {
+            Ok(searcher) => searcher,
+            Err(err) => {
+                searcher_err = Some(err);
+                return Box::new(move |_| WalkState::Quit);
+            }
+        };
+
+        Box::new(move |result| {
+            let subject = match subject_builder.build_from_result(result) {
+                Some(subject) => subject,
+                None => return WalkState::Continue,
+            };
+            searched.store(true, SeqCst);
+            searcher.printer().get_mut().clear();
+            let search_result = match searcher.search(&subject) {
+                Ok(search_result) => search_result,
+                Err(err) => {
+                    err_message!("{}: {}", subject.path().display(), err);
+                    return WalkState::Continue;
+                }
+            };
+            if search_result.has_match() {
+                matched.store(true, SeqCst);
+            }
+            if let Some(ref locked_stats) = *stats {
+                let mut stats = locked_stats.lock().unwrap();
+                *stats += search_result.stats().unwrap();
+            }
+            if let Err(err) = bufwtr.print(searcher.printer().get_mut()) {
+                // A broken pipe means graceful termination.
+                if err.kind() == io::ErrorKind::BrokenPipe {
+                    return WalkState::Quit;
+                }
+                // Otherwise, we continue on our merry way.
+                err_message!("{}: {}", subject.path().display(), err);
+            }
+            if matched.load(SeqCst) && quit_after_match {
+                WalkState::Quit
+            } else {
+                WalkState::Continue
+            }
+        })
+    });
+    if let Some(err) = searcher_err.take() {
+        return Err(err);
+    }
+    if args.using_default_path() && !searched.load(SeqCst) {
+        eprint_nothing_searched();
+    }
+    if let Some(ref locked_stats) = stats {
+        let elapsed = Instant::now().duration_since(started_at);
+        let stats = locked_stats.lock().unwrap();
+        let mut searcher = args.search_worker(args.stdout())?;
+        // We don't care if we couldn't print this successfully.
+        let _ = searcher.print_stats(elapsed, &stats);
+    }
+    Ok(matched.load(SeqCst))
+}
---
+++ src/patch.rs
@@ -0,0 +1,350 @@
+use std::io;
+use std::os::unix::prelude::OsStrExt;
+use std::path::Path;
+use std::time::Instant;
+
+use grep::matcher::{Matcher, Match};
+use grep::printer::util::{find_iter_at_in_context, Replacer};
+use grep::searcher::{Searcher, Sink, SinkContextKind, SinkMatch, SinkContext, SinkFinish};
+
+use crate::patcht::{PatchHunk, PatchStyle};
+
+const ORIG_PREFIX: &[u8] = b"--- ";
+const MOD_PREFIX: &[u8] = b"+++ ";
+
+#[derive(Debug, Clone)]
+struct Config {
+    // Patch printing can only be used with a replacement.
+    replacement: Vec<u8>,
+    style: PatchStyle,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config { style: PatchStyle::Unified, replacement: Vec::default(), }
+    }
+}
+
+/// Configuration for the patch-output printer
+#[derive(Clone, Debug, Default)]
+pub struct PatchBuilder {
+    config: Config,
+}
+
+impl PatchBuilder {
+    /// Return a new builder for configuring the patch printer.
+    pub fn new() -> PatchBuilder {
+        PatchBuilder::default()
+    }
+
+    /// Create a Patch printer that writes results to the given writer.
+    pub fn build<W: io::Write>(&self, wtr: W) -> Patch<W> {
+        Patch {
+            config: self.config.clone(),
+            wtr: wtr,
+            matches: vec![],
+        }
+    }
+
+    /// Set the bytes that will be used to replace each occurrence of a match
+    /// found.
+    ///
+    /// The replacement bytes given may include references to capturing groups,
+    /// which may either be in index form (e.g., `$2`) or can reference named
+    /// capturing groups if present in the original pattern (e.g., `$foo`).
+    ///
+    /// For documentation on the full format, please see the `Capture` trait's
+    /// `interpolate` method in the
+    /// [grep-printer](https://docs.rs/grep-printer) crate.
+    pub fn replacement(
+        &mut self,
+        replacement: Vec<u8>,
+    ) -> &mut PatchBuilder {
+        self.config.replacement = replacement;
+        self
+    }
+}
+
+/// A printer for generating patch output, usable with the POSIX `patch`
+/// utility.
+#[derive(Debug)]
+pub struct Patch<W> {
+    config: Config,
+    wtr: W,
+    matches: Vec<Match>,
+}
+
+impl<W> Patch<W> {
+    /// Returns true if and only if this printer has written at least one byte
+    /// to the underlying writer during any of the previous searches.
+    pub fn has_written(&self) -> bool {
+        self.wtr.total_count() > 0
+    }
+
+    /// Return a mutable reference to the underlying writer.
+    pub fn get_mut(&mut self) -> &mut W {
+        self.wtr.get_mut()
+    }
+
+    /// Consume this printer and return back ownership of the underlying
+    /// writer.
+    pub fn into_inner(self) -> W {
+        self.wtr.into_inner()
+    }
+}
+
+/// An implementation of `Sink` associated with a matcher and an optional file
+/// path for the patch printer.
+///
+/// A `Sink` can be created via the
+/// [`Standard::sink`](struct.Standard.html#method.sink)
+/// or
+/// [`Standard::sink_with_path`](struct.Standard.html#method.sink_with_path)
+/// methods, depending on whether you want to include a file path in the
+/// printer's output.
+#[derive(Debug)]
+pub struct PatchSink<'p, 's, M: Matcher, W> {
+    matcher: M,
+    patch: &'s mut Patch<W>,
+    replacer: Replacer<M>,
+    current_hunk: Option<PatchHunk>,
+    path: &'p Path,
+    start_time: Instant,
+    match_count: u64,
+    after_context_remaining: u64,
+    binary_byte_offset: Option<u64>,
+    begin_printed: bool,
+}
+
+impl<'p, 's, M: Matcher, W: io::Write> PatchSink<'p, 's, M, W> {
+    /// Returns true if and only if this printer received a match in the
+    /// previous search.
+    ///
+    /// This is unaffected by the result of searches before the previous
+    /// search on this sink.
+    pub fn has_match(&self) -> bool {
+        return self.match_count > 0;
+    }
+
+    /// Execute the matcher over the given bytes and record the match
+    /// locations if the current configuration demands match granularity.
+    fn record_matches(
+        &mut self,
+        searcher: &Searcher,
+        bytes: &[u8],
+        range: std::ops::Range<usize>,
+    ) -> io::Result<()> {
+        self.patch.matches.clear();
+
+        // Implementation taken from `Standard.record_matches`; see comment
+        // there about allocation
+        let matches = &mut self.patch.matches;
+        find_iter_at_in_context(
+            searcher,
+            &self.matcher,
+            bytes,
+            range.clone(),
+            |m| {
+                let (s, e) = (m.start() - range.start, m.end() - range.start);
+                matches.push(Match::new(s, e));
+                true
+            },
+        )?;
+        // Don't report empty matches appearing at the end of the bytes.
+        if !matches.is_empty()
+            && matches.last().unwrap().is_empty()
+            && matches.last().unwrap().start() >= range.end
+        {
+            matches.pop().unwrap();
+        }
+        Ok(())
+    }
+
+    /// The Patch printer performs replacement unconditionally.
+    fn replace(
+        &mut self,
+        searcher: &Searcher,
+        bytes: &[u8],
+        range: std::ops::Range<usize>,
+    ) -> io::Result<()> {
+        self.replacer.clear();
+        self.replacer.replace_all(
+            searcher,
+            &self.matcher,
+            bytes,
+            range,
+            &self.patch.config.replacement,
+        )
+    }
+
+    /// Write the patch header, which includes the name and timestamp of the
+    /// current file
+    fn write_patch_header(&mut self) -> io::Result<()> {
+        if self.begin_printed {
+            return Ok(());
+        }
+        // XXX need to select header type based on config style
+        write_header(&mut self.patch.wtr, self.path)?;
+        self.begin_printed = true;
+        Ok(())
+    }
+}
+
+fn write_header<W: io::Write>(wtr: &mut W, path: &Path) -> io::Result<()> {
+    // XXX for this, should the 'file2' path be different from 'file1'?
+    let path_bytes = path.as_os_str().as_bytes();
+    wtr.write(ORIG_PREFIX)?;
+    wtr.write(path_bytes)?;
+    // The GNU and POSIX documentation both state that diffs include file
+    // timestamps, but git doesn't include one with either `diff` or
+    // `format-patch`.
+    // XXX figure out if this actually matters
+    // fs::metadata(self.path)?.modified()?
+    // wtr.write(b", 2002-02-21 23:30:39.942229878 -0800")?;
+
+    // XXX should the line-endings for patch files match the native line-endings?
+    // Will this be done automatically? (See comment in `json.rs`)
+    wtr.write(&[b'\n'])?;
+    wtr.write(MOD_PREFIX)?;
+    wtr.write(path_bytes)?;
+    // XXX see comment above about the timestamps
+    // wtr.write(b", 2002-02-21 23:30:39.942229878 -0800")?;
+    wtr.write(&[b'\n'])?;
+    Ok(())
+}
+
+impl<'p, 's, M: Matcher, W: io::Write> Sink for PatchSink<'p, 's, M, W> {
+    type Error = io::Error;
+
+    fn matched(
+        &mut self,
+        searcher: &Searcher,
+        mat: &SinkMatch<'_>,
+    ) -> Result<bool, io::Error> {
+        self.write_patch_header()?;
+
+        self.match_count += 1;
+
+        self.record_matches(
+            searcher,
+            mat.buffer(),
+            mat.bytes_range_in_buffer(),
+        )?;
+        self.replace(searcher, mat.buffer(), mat.bytes_range_in_buffer())?;
+
+        if searcher.binary_detection().convert_byte().is_some() {
+            if self.binary_byte_offset.is_some() {
+                return Ok(false);
+            }
+        }
+
+        let hunk = self.current_hunk.get_or_insert(PatchHunk::default());
+        hunk.add_match(
+            mat, self.replacer.replacement().expect("no replacement occurred").0);
+
+        Ok(true)
+    }
+
+    fn context(
+        &mut self,
+        searcher: &Searcher,
+        ctx: &SinkContext<'_>,
+    ) -> Result<bool, io::Error> {
+        self.patch.matches.clear();
+        self.replacer.clear();
+
+        if ctx.kind() == &SinkContextKind::After {
+            self.after_context_remaining =
+                self.after_context_remaining.saturating_sub(1);
+        }
+        if searcher.binary_detection().convert_byte().is_some() {
+            if self.binary_byte_offset.is_some() {
+                return Ok(false);
+            }
+        }
+
+        let hunk = self.current_hunk.get_or_insert(PatchHunk::default());
+        hunk.add_context(ctx);
+
+        return Ok(true)
+    }
+
+    fn context_break(
+        &mut self,
+        _: &Searcher,
+    ) -> Result<bool, io::Error> {
+        if let Some(previous) = &mut self.current_hunk {
+            previous.write(&mut self.patch.wtr, self.patch.config.style)?;
+        }
+        self.current_hunk = Some(PatchHunk::default());
+        Ok(true)
+    }
+
+    fn binary_data(
+        &mut self,
+        _searcher: &Searcher,
+        binary_byte_offset: u64,
+    ) -> Result<bool, io::Error> {
+        self.binary_byte_offset = Some(binary_byte_offset);
+        Ok(true)
+    }
+
+
+    fn begin(&mut self, _searcher: &Searcher) -> Result<bool, io::Error> {
+        self.patch.wtr.reset_count();
+        self.start_time = Instant::now();
+        self.match_count = 0;
+        self.after_context_remaining = 0;
+        self.binary_byte_offset = None;
+        Ok(true)
+    }
+
+    fn finish(
+        &mut self,
+        _searcher: &Searcher,
+        finish: &SinkFinish,
+    ) -> Result<(), io::Error> {
+        if !self.begin_printed {
+            return Ok(());
+        }
+
+        if let Some(previous) = &mut self.current_hunk {
+            previous.write(&mut self.patch.wtr, self.patch.config.style)?;
+        }
+
+        self.binary_byte_offset = finish.binary_byte_offset();
+
+        Ok(())
+    }
+}
+
+impl<W: io::Write> Patch<W> {
+    /// Return an implementation of `Sink` associated with a file path.
+    ///
+    /// When the printer is associated with a path, then it may, depending on
+    /// its configuration, print the path along with the matches found.
+    pub fn sink_with_path<'p, 's, M, P>(
+        &'s mut self,
+        matcher: M,
+        path: &'p P,
+    ) -> PatchSink<'p, 's, M, W>
+    where
+        M: Matcher,
+        P: ?Sized + AsRef<Path>,
+    {
+        PatchSink {
+            matcher: matcher,
+            patch: self,
+            replacer: Replacer::new(),
+            current_hunk: None,
+            path: path.as_ref(),
+            start_time: Instant::now(),
+            match_count: 0,
+            after_context_remaining: 0,
+            binary_byte_offset: None,
+            begin_printed: false,
+        }
+    }
+
+
+}
